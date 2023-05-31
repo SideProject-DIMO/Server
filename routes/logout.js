2cc6e750bf3e14b0516f05744e9fb896483d0f9c
plum-king <jeesoo0908@gmail.com>
2023-05-31 18:42:17 +0900

    #1 [Feat] 로그아웃 api 수정

--- routes/logout.js
+++ routes/logout.js
@@ -4,10 +4,24 @@ const router = express.Router();
 
 // logout
 router.get("/logout", async (req, res) => {
-  res.clearCookie("access_token");
-  res.clearCookie("refresh_token");
-  res.send();
-  console.log("로그아웃");
+  let resultCode = 404;
+  let message = "에러가 발생했습니다";
+
+  try {
+    res.clearCookie("access_token");
+    res.clearCookie("refresh_token");
+
+    resultCode = 200;
+    message = "로그아웃에 성공하였습니다";
+
+    return res.json({
+      code: resultCode,
+      message: message,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
 });
 
 module.exports = router;
