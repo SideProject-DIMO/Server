ebec0751c442d6989340760b48bcfac767f1684d
Jay Kim <jayfuljay9@gmail.com>
2023-06-27 01:23:52 +0900

    movie_home,detail initial commit

--- app.js
+++ app.js
@@ -1,34 +1,37 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-let bodyParser = require("body-parser");
+let bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get("/", async (req, res) => {
-  res.send("success");
+app.get('/', async (req, res) => {
+  res.send('success');
 });
 
 //회원가입
-app.use("/signup", require("./routes/dimo_signup.js"));
+app.use('/signup', require('./routes/dimo_signup.js'));
 
 //로그인&로그아웃&탈퇴
-app.use("/social", require("./routes/social_login.js"));
-app.post("/login", require("./routes/standard_login.js"));
-app.get("/logout", require("./routes/logout"));
-app.post("/drop", require("./routes/drop.js"));
+app.use('/social', require('./routes/social_login.js'));
+app.post('/login', require('./routes/standard_login.js'));
+app.get('/logout', require('./routes/logout'));
+app.post('/drop', require('./routes/drop.js'));
 
 //회원정보변경
-app.use("/user_info", require("./routes/change_user_info"));
+app.use('/user_info', require('./routes/change_user_info'));
 
 //크롤링
-app.use("/crawling", require("./routes/anime_data_crawling.js"));
+app.use('/crawling', require('./routes/anime_data_crawling.js'));
+
+//영화 home
+app.use('/home', require('./routes/movie_home.js'));
 
 //상세 조회
-app.use("/detail", require("./routes/detail.js"));
+app.use('/detail', require('./routes/detail.js'));
 
-app.use(require("./routes/auth_middleware"));
+app.use(require('./routes/auth_middleware'));
 
 app.listen(3000, function () {
-  console.log("server is running.");
+  console.log('server is running.');
 });
--- routes/detail.js
+++ routes/detail.js
@@ -1,19 +1,19 @@
-const pool = require("../db");
-const puppeteer = require("puppeteer");
-const express = require("express");
+const pool = require('../db');
+const puppeteer = require('puppeteer');
+const express = require('express');
 const router = express.Router();
 
-router.post("/like", async (req, res, next) => {
-  const {user_id, content_type, contentId} = req.body;
+router.post('/like', async (req, res, next) => {
+  const { user_id, content_type, contentId } = req.body;
   let result_code = 404;
-  let message = "에러가 발생했습니다.";
+  let message = '에러가 발생했습니다.';
   try {
     const [like] = await pool.execute(
       `INSERT INTO dimo_like (content_type, content_id, user_id) VALUES (?, ?, ?)`,
       [content_type, contentId, user_id]
     );
     result_code = 200;
-    message = "좋아요를 눌렀습니다.";
+    message = '좋아요를 눌렀습니다.';
     return res.json({
       code: result_code,
       message: message,
@@ -25,17 +25,17 @@ router.post("/like", async (req, res, next) => {
   }
 });
 
-router.post("/dislike", async (req, res, next) => {
-  const {user_id, content_type, contentId} = req.body;
+router.post('/dislike', async (req, res, next) => {
+  const { user_id, content_type, contentId } = req.body;
   let result_code = 404;
-  let message = "에러가 발생했습니다.";
+  let message = '에러가 발생했습니다.';
   try {
     const [dislike] = await pool.execute(
       `DELETE FROM dimo_like WHERE user_id = ? and content_type = ? and content_id = ?`,
       [user_id, content_type, contentId]
     );
     result_code = 200;
-    message = "좋아요를 취소했습니다.";
+    message = '좋아요를 취소했습니다.';
     return res.json({
       code: result_code,
       message: message,
@@ -47,7 +47,7 @@ router.post("/dislike", async (req, res, next) => {
   }
 });
 
-router.get("/animedata/:contentId", async (req, res, next) => {
+router.get('/animedata/:contentId', async (req, res, next) => {
   try {
     let contentId = req.params.contentId;
     let [detail] = await pool.execute(
@@ -57,18 +57,18 @@ router.get("/animedata/:contentId", async (req, res, next) => {
 
     console.log(detail[0].url_type);
 
-    let url = "https://anime.onnada.com/";
+    let url = 'https://anime.onnada.com/';
     if (detail[0].url_type == 0) {
-      url += contentId + "/nav/good";
+      url += contentId + '/nav/good';
     } else if (detail[0].url_type == 2) {
-      url += contentId + "/nav/quarter";
+      url += contentId + '/nav/quarter';
     } else {
       url += contentId;
     }
     // 크롤링 시작
     const browser = await puppeteer.launch({
       headless: true,
-      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
 
     const results = [];
@@ -76,33 +76,33 @@ router.get("/animedata/:contentId", async (req, res, next) => {
     await page.goto(url.trim());
 
     // 포스터 이미지
-    const posterElement = await page.$(".view-info .image img");
+    const posterElement = await page.$('.view-info .image img');
     const posterImg = await page.evaluate(
-      (element) => element.getAttribute("src"),
+      (element) => element.getAttribute('src'),
       posterElement
     );
 
     // 프로필+캐릭터명, 작품명, 줄거리
-    const grabData = await page.$$eval(".view-chacon li", (items) => {
+    const grabData = await page.$$eval('.view-chacon li', (items) => {
       const data = [];
 
       for (const item of items) {
-        const photoDiv = item.querySelector(".photo");
-        const listDiv = item.querySelector(".list");
-        let characterImg = "";
-        let characterName = "";
+        const photoDiv = item.querySelector('.photo');
+        const listDiv = item.querySelector('.list');
+        let characterImg = '';
+        let characterName = '';
 
         if (photoDiv) {
-          const img = photoDiv.querySelector("img");
+          const img = photoDiv.querySelector('img');
           if (img) {
-            characterImg = img.getAttribute("src");
+            characterImg = img.getAttribute('src');
           }
         }
 
         if (listDiv) {
-          const box1 = listDiv.querySelector(".box1");
+          const box1 = listDiv.querySelector('.box1');
           if (box1) {
-            characterName = box1.querySelector(".name").textContent;
+            characterName = box1.querySelector('.name').textContent;
           }
         }
 
@@ -112,11 +112,11 @@ router.get("/animedata/:contentId", async (req, res, next) => {
         });
       }
 
-      const titleTag = document.querySelector(".view-title h1");
-      const title = titleTag ? titleTag.innerHTML : "";
+      const titleTag = document.querySelector('.view-title h1');
+      const title = titleTag ? titleTag.innerHTML : '';
 
-      const plotTag = document.querySelector(".c");
-      const plot = plotTag ? plotTag.innerHTML : "";
+      const plotTag = document.querySelector('.c');
+      const plot = plotTag ? plotTag.innerHTML : '';
 
       return {
         title,
@@ -180,8 +180,89 @@ router.get("/animedata/:contentId", async (req, res, next) => {
 
     res.json(results[0]);
   } catch (error) {
-    console.error("Error during crawling:", error);
-    res.status(500).send("Error during crawling");
+    console.error('Error during crawling:', error);
+    res.status(500).send('Error during crawling');
+  }
+});
+
+router.get('/moviedata/:movieId', async (req, res) => {
+  try {
+    // 영화 ID
+    const movieId = req.query.movieId; // 요청 쿼리 파라미터로부터 영화 ID 받기
+
+    // TMDB API 키
+    const apiKey = '9e43a6867c994baa79d59e3d65755b86';
+
+    // TMDB API 요청 URL
+    const apiUrl = `https://api.themoviedb.org/3`;
+
+    // 영화 정보를 가져오는 함수
+    async function getMovieInfo(movieId) {
+      try {
+        // 영화 상세 정보 요청
+        const response = await axios.get(
+          `${apiUrl}/movie/${movieId}?language=ko`,
+          {
+            params: {
+              api_key: apiKey,
+            },
+          }
+        );
+
+        // 감독 및 캐릭터 정보 요청
+        const creditsResponse = await axios.get(
+          `${apiUrl}/movie/${movieId}/credits?language=ko`,
+          {
+            params: {
+              api_key: apiKey,
+            },
+          }
+        );
+
+        // 영화 정보
+        const movieInfo = response.data;
+        const title = movieInfo.title;
+        const plot = movieInfo.overview;
+        const release_date = movieInfo.release_date;
+        const genres = movieInfo.genres;
+        const runtime = movieInfo.runtime;
+        const posterImg = movieInfo.poster_path;
+        const movieID = movieInfo.belongs_to_collection.id;
+
+        // 감독
+        const director = creditsResponse.data.crew.find(
+          (person) => person.job === 'Director'
+        );
+
+        // 캐릭터 정보
+        const characters = creditsResponse.data.cast.slice(0, 15); // 상위 15명의 캐릭터 정보만
+
+        // 영화 정보 return
+        return {
+          title,
+          plot,
+          release_date,
+          genres,
+          runtime,
+          posterImg,
+          movieID,
+          director: director.name,
+          characters: characters.map((character) => ({
+            profileImg: character.profile_path,
+            characterName: character.character,
+          })),
+        };
+      } catch (error) {
+        throw new Error(error.message);
+      }
+    }
+
+    // 영화 정보 가져오기
+    const movieInfo = await getMovieInfo(movieId);
+
+    res.json(movieInfo);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 });
 
