faba1ff9623a8a50387a1042c81d56daa03036a2
plum-king <jeesoo0908@gmail.com>
2023-07-08 23:17:13 +0900

    #48 [Mod] detail 평점 관련 로직 추가

--- routes/detail.js
+++ routes/detail.js
@@ -56,61 +56,94 @@ router.post("/grade", async (req, res, next) => {
   let grade_message = "평점 평균을 저장하는데 에러가 발생했습니다.";
 
   try {
+    // 내 mbti
     const [my_mbti] = await pool.execute(
       `SELECT mbti FROM user WHERE user_id = ?`,
       [user_id]
     );
 
+    // 평점을 누른 적이 있는지
     const [is_grade_dup] = await pool.execute(
       `SELECT * FROM dimo_grade WHERE user_id = ? and content_id =? and content_type =?`,
       [user_id, contentId, content_type]
     );
 
+    // 내 mbti에 해당하는 평점 평균이 존재하는지
+    let [is_exist_grade] = await pool.execute(
+      `SELECT * FROM dimo_grade_avg WHERE content_id = ? and content_type = ? and mbti = ?`,
+      [contentId, content_type, my_mbti[0].mbti]
+    );
+
+    // 내가 평점을 누른 적이 없다면
     if (is_grade_dup[0] == null) {
+      // 평점 저장
       const [post_grade] = await pool.execute(
         `INSERT INTO dimo_grade (grade, user_id, content_id, content_type) VALUES (?, ?, ?, ?)`,
         [grade, user_id, contentId, content_type]
       );
       result_code = 200;
       message = "평점을 저장했습니다.";
-    } else {
-      const [mod_grade] = await pool.execute(
-        `UPDATE dimo_grade SET grade =? WHERE user_id = ? and content_id = ? and content_type = ?`,
-        [grade, user_id, contentId, content_type]
-      );
-      result_code = 201;
-      message = "평점을 수정했습니다.";
-    }
 
-    let [is_exist_grade] = await pool.execute(
-      `SELECT * FROM dimo_grade_avg WHERE content_id = ? and content_type = ? and mbti = ?`,
-      [contentId, content_type, my_mbti[0].mbti]
-    );
-
+      // 나와 동일한 mbti인 사람이 이미 해당 콘텐츠에 대해 평점을 누른 적이 있다면
       if (is_exist_grade[0] != null) {
         await pool.execute(
-        `UPDATE dimo_grade_avg SET mbti_grade_avg = ? and avg_people = ? WHERE content_id = ? and content_type = ? and mbti = ?`,
+          `UPDATE dimo_grade_avg SET mbti_grade_avg = ? and avg_people = ? and mbti_grade_sum = ? WHERE content_id = ? and content_type = ? and mbti = ?`,
           [
-          is_exist_grade[0].mbti_grade_avg + grade,
+            (is_exist_grade[0].mbti_grade_sum + grade) /
+              (is_exist_grade[0].avg_people + 1),
             is_exist_grade[0].avg_people + 1,
+            is_exist_grade[0].mbti_grade_sum + grade,
             contentId,
             content_type,
             my_mbti[0].mbti,
           ]
         );
         grade_message = "평점 평균을 업데이트 했습니다.";
+
+        // 나와 동일한 mbti인 사람이 한 번도 콘텐츠에 대해 평점을 누른 적이 없다면
       } else {
         await pool.execute(
-        `INSERT INTO dimo_grade_avg (content_id, mbti, mbti_grade_avg, avg_people, content_type) VALUES (?, ?, ?, ?, ?)`,
-        [conetentId, my_mbti[0].mbti, grade, 1, content_type]
+          `INSERT INTO dimo_grade_avg (content_id, mbti, mbti_grade_avg, mbti_grade_sum, avg_people, content_type) VALUES (?, ?, ?, ?, ?, ?)`,
+          [contentId, my_mbti[0].mbti, grade, grade, 1, content_type]
         );
         grade_message = "평점 평균을 추가했습니다.";
       }
       grade_result_code = 200;
 
+      // 내가 평점을 누른 적이 있다면
+    } else {
+      let last_grade = parseFloat(is_grade_dup[0].grade); // 이전 평점
+      let now_grade_sum =
+        parseFloat(is_exist_grade[0].mbti_grade_sum) - last_grade + grade; // 현재 평점 총합
+      let now_grade_avg =
+        (is_exist_grade[0].mbti_grade_sum - last_grade + grade) /
+        is_exist_grade[0].avg_people;
+
+      console.log(now_grade_avg);
+      console.log(now_grade_sum);
+
+      // 전에 있던 평점에서 업데이트
+      const [mod_grade] = await pool.execute(
+        `UPDATE dimo_grade SET grade =? WHERE user_id = ? and content_id = ? and content_type = ?`,
+        [grade, user_id, contentId, content_type]
+      );
+      result_code = 201;
+      message = "평점을 수정했습니다.";
+
+      // 내가 이미 평점을 눌러 내 mbti는 평점 평균이 존재하므로 사람 수는 변화 없이 평점 총합에서 이전 평점을 빼고 새롭게 변동한 평점을 더해서 저장
+      await pool.execute(
+        `UPDATE dimo_grade_avg SET mbti_grade_avg = ? and mbti_grade_sum = ? WHERE content_id = ? and content_type = ? and mbti = ?`,
+        [now_grade_avg, now_grade_sum, contentId, content_type, my_mbti[0].mbti]
+      );
+      grade_message = "평점 평균을 수정했습니다.";
+      grade_result_code = 201;
+    }
+
     return res.json({
       code: result_code,
       message: message,
+      grade_result_code: grade_result_code,
+      grade_message: grade_message,
       user_id: user_id,
       content_type: content_type,
       conetentId: contentId,
