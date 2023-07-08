c2b389e8cf9eaeefef91e75fdb9242ee326e1260
plum-king <jeesoo0908@gmail.com>
2023-07-08 23:45:45 +0900

    #7 [Fix] mbti 수정 오류 픽스

--- routes/change_user_info.js
+++ routes/change_user_info.js
@@ -214,7 +214,10 @@ router.get("/change_mbti", async (req, res, next) => {
 
     let now = new Date();
 
-    if (confirm_mbti[0].mbti.toUpperCase() == mbti.toUpperCase()) {
+    if (
+      (confirm_mbti[0] != null) &
+      (confirm_mbti[0].mbti.toUpperCase() == mbti.toUpperCase())
+    ) {
       resultCode = 401;
       message = "바꾸려는 mbti가 동일합니다.";
       return res.json({
