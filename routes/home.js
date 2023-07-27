5b779bf576476a6bf0f2cfb246aa553afbcdac7e
plum-king <jeesoo0908@gmail.com>
2023-07-28 03:31:37 +0900

    #70 [Feat] anime home

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,10 @@
 const pool = require("../db");
-const puppeteer = require("puppeteer");
 const express = require("express");
 const router = express.Router();
+const axios = require("axios");
+require("dotenv").config();
 
-router.get("/", async (req, res, next) => {
+router.get("/anime", async (req, res, next) => {
   const user_id = req.query.user_id;
   let result_code = 400;
   let message = "에러가 발생했습니다.";
@@ -17,12 +18,31 @@ router.get("/", async (req, res, next) => {
     let [represent] = await pool.execute(
       `SELECT * FROM anime_contents WHERE id = ? or id = ? or id = ? or id = ? or id= ?`,
       [rand_num[0], rand_num[1], rand_num[2], rand_num[3], rand_num[4]]
-    ); //랜덤 콘텐츠 
+    ); //랜덤 애니
+
+    let [my_mbti] = await pool.execute(
+      `SELECt mbti FROM user WHERE user_id = ?`,
+      [user_id]
+    );
+
+    let [same_mbti_anime] = await pool.execute(
+      `SELECT DISTINCT * FROM anime_contents WHERE anime_id IN (SELECT anime_id FROM anime_character WHERE character_mbti = ?) LIMIT 10`,
+      [my_mbti[0].mbti]
+    ); //같은 mbti의 주인공이 있는 애니
+
+    let [same_mbti_char] = await pool.execute(
+      `SELECT * FROM anime_character WHERE character_mbti = ? LIMIT 10`,
+      [my_mbti[0].mbti]
+    ); //같은 mbti인 캐릭터 모아보기
 
     let [recommend] = await pool.execute(
-      `SELECT * FROM anime_contents WHERE anime_id in (SELECT content_id FROM dimo_like WHERE user_id in (SELECT user_id FROM user WHERE mbti = (SELECT mbti FROM user WHERE user_id = ?))) LIMIT 10`,
-      [user_id]
-    ); //같은 mbti 사용자가 추천한 영화
+      `SELECT anime_id, title, genre, plot, poster_img, director, anime_release, rate, hits FROM anime_contents WHERE anime_id in (SELECT DISTINCT content_id FROM dimo_like WHERE user_id in (SELECT user_id FROM user WHERE mbti = ?)) LIMIT 10`,
+      [my_mbti[0].mbti]
+    ); //같은 mbti 사용자가 추천한 애니
+
+    let [hit] = await pool.execute(
+      `SELECT * FROM anime_contents ORDER BY hits DESC LIMIT 10`
+    ); //조회수 순 정렬
 
     result_code = 200;
     message = "홈 화면 조회에 성공했습니다.";
@@ -30,8 +50,26 @@ router.get("/", async (req, res, next) => {
       code: result_code,
       message: message,
       contents: [
-        {category: "represent", represent},
-        {category: "recommend", recommend},
+        {
+          category: "represent",
+          represent,
+        },
+        {
+          category: "same_mbti_anime",
+          same_mbti_anime,
+        },
+        {
+          category: "same_mbti_character",
+          same_mbti_char,
+        },
+        {
+          category: "recommend",
+          recommend,
+        },
+        {
+          category: "hits",
+          hit,
+        },
       ],
     });
   } catch (error) {
@@ -39,5 +77,79 @@ router.get("/", async (req, res, next) => {
     return res.status(500).json(error);
   }
 });
+
+// router.get("/movie", async (req, res) => {
+//   const user_id = req.query.user_id;
+
+//   try {
+//     let rand_num = [];
+//     let movieI = [];
+
+//     for (let i = 0; i < 5; i++) {
+//       rand_num[i] = Math.floor(Math.random() * (100 - 1) + 1); //1~100까지 랜덤 선택
+//     }
+
+//     let [represent] = await pool.execute(
+//       `SELECT * FROM movie_contents WHERE id = ? or id = ? or id = ? or id = ? or id= ?`,
+//       [rand_num[0], rand_num[1], rand_num[2], rand_num[3], rand_num[4]]
+//     ); //랜덤 콘텐츠
+//     // const movie_id = req.query.movie_id; // 요청 쿼리 파라미터로부터 영화 ID 받기
+
+//     // TMDB API 요청 URL
+//     const apiUrl = `https://api.themoviedb.org/3`;
+
+//     // 영화 정보를 가져오는 함수
+//     // async function getRandInfo(represent) {
+//     //   try {
+//     for (let i of represent) {
+//       console.log(i.id);
+//       // 영화 상세 정보 요청
+//       const response = await axios.get(`${apiUrl}/movie/${i.id}?language=ko`, {
+//         params: {
+//           api_key: process.env.api_key,
+//         },
+//       });
+
+//       // 영화 정보
+//       const movie_info = response.data;
+//       const title = movie_info.title;
+//       const poster_img = movie_info.poster_path;
+//       const id = movie_info.id;
+//       console.log(title);
+
+//       // console.log(id);
+
+//       // movieI.push([title, poster_img, id]); //이 부분 안됨
+//       // console.log(movieI);
+
+//       // // 영화 정보 return
+//       // return {
+//       //   title,
+//       //   poster_img,
+//       //   id,
+//       // };
+//       // }
+//       // } catch (error) {
+//       //   // console.error(error);
+//       //   throw new Error(error.message);
+//       // }
+//     }
+
+//     // movie_info = await getRandInfo(represent);
+//     // console.log(movie_info);
+//     // 영화 정보 랜덤으로 가져오기
+//     // for (let i of represent) {
+//     //   movie_info = await getRandInfo(i.id);
+//     //   console.log(movie_info);
+//     // }
+
+//     // res.json(movieI);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({
+//       error: error.message,
+//     });
+//   }
+// });
 
 module.exports = router;
