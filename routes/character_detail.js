4f85abd74343477a051a6ae289f80b2352eb3827
plum-king <jeesoo0908@gmail.com>
2023-07-28 23:31:57 +0900

    #48 [Feat] add review good, good cancel, delete

--- routes/character_detail.js
+++ routes/character_detail.js
@@ -17,7 +17,7 @@ router.get("/", async (req, res, next) => {
       [character_id]
     );
     result_code = 200;
-    message = "리뷰 조회 성공";
+    message = "리뷰를 조회했습니다.";
     return res.json({
       code: result_code,
       message: message,
@@ -41,7 +41,104 @@ router.post("/write_review", async (req, res, next) => {
       [user_id, character_id, review_content, review_spoiler]
     );
     result_code = 200;
-    message = "리뷰 작성 성공";
+    message = "리뷰를 작성했습니다.";
+    return res.json({
+      code: result_code,
+      message: message,
+      user_id: user_id,
+      character_id: character_id,
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json(error);
+  }
+});
+
+router.post("/review_like", async (req, res, next) => {
+  //리뷰 좋아요 누르기
+  const {user_id, character_id, review_id} = req.body;
+  let result_code = 400;
+  let message = "에러가 발생했습니다.";
+  try {
+    let [is_like] = await pool.execute(
+      `SELECT * FROM review_like WHERE user_id = ? and review_id = ?`,
+      [user_id, review_id]
+    );
+
+    if (is_like[0] == null) {
+      await pool.execute(
+        `INSERT INTO review_like (character_id, user_id, review_id) VALUES (?, ?, ?)`,
+        [character_id, user_id, review_id]
+      );
+      await pool.execute(
+        `UPDATE character_review SET review_like = review_like + 1 WHERE review_id = ?`,
+        [review_id]
+      );
+      result_code = 200;
+      message = "좋아요를 눌렀습니다.";
+    } else {
+      result_code = 201;
+      message = "이미 좋아요를 누른 리뷰입니다.";
+    }
+
+    return res.json({
+      code: result_code,
+      message: message,
+      user_id: user_id,
+      character_id: character_id,
+      review_id: review_id,
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json(error);
+  }
+});
+
+router.post("/review_dislike", async (req, res, next) => {
+  //리뷰 좋아요 취소하기
+  const {user_id, character_id, review_id} = req.body;
+  let result_code = 400;
+  let message = "에러가 발생했습니다.";
+  try {
+    await pool.execute(
+      `DELETE FROM review_like WHERE review_id = ? and user_id = ?`,
+      [review_id, user_id]
+    );
+    await pool.execute(
+      `UPDATE character_review SET review_like = review_like - 1 WHERE review_id = ?`,
+      [review_id]
+    );
+    result_code = 200;
+    message = "좋아요를 취소했습니다.";
+
+    return res.json({
+      code: result_code,
+      message: message,
+      user_id: user_id,
+      character_id: character_id,
+      review_id: review_id,
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json(error);
+  }
+});
+
+router.delete("/delete_review", async (req, res, next) => {
+  //리뷰 삭제하기
+  const {user_id, character_id, review_id} = req.body;
+  let result_code = 400;
+  let message = "에러가 발생했습니다.";
+  try {
+    await pool.execute(`DELETE FROM review_like WHERE review_id = ?`, [
+      review_id,
+    ]);
+    await pool.execute(`DELETE FROM character_review WHERE review_id = ?`, [
+      review_id,
+    ]);
+    result_code = 200;
+    message = "리뷰를 삭제했습니다.";
+
     return res.json({
       code: result_code,
       message: message,
