bad58031e477bc8d0a8976b186f948ebf7757aee
plum-king <jeesoo0908@gmail.com>
2023-08-06 01:01:57 +0900

    #1 [Feat] add find id

--- routes/standard_login.js
+++ routes/standard_login.js
@@ -5,7 +5,7 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-router.post("/login", async (req, res, next) => {
+router.post("/", async (req, res, next) => {
   const {user_id, password} = req.body;
   let resultCode = 404;
   let message = "에러가 발생했습니다";
@@ -25,7 +25,7 @@ router.post("/login", async (req, res, next) => {
         message: message,
         user_id: user_id,
         access_token: access_token,
-        refresh_token: ""
+        refresh_token: "",
       });
     } else if (!bcrypt.compareSync(password, data[0][0].password)) {
       // 비밀번호가 다르다면
@@ -37,7 +37,7 @@ router.post("/login", async (req, res, next) => {
         message: message,
         user_id: user_id,
         access_token: access_token,
-        refresh_token: ""
+        refresh_token: "",
       });
     } else {
       // 다른 경우는 없다고 판단하여 성공
@@ -51,7 +51,9 @@ router.post("/login", async (req, res, next) => {
           nickname: data[0][0].nickname,
         },
         process.env.jwt_secret,
-        {expiresIn: "1h"} //만료 시간 1시간
+        {
+          expiresIn: "1h",
+        } //만료 시간 1시간
       );
 
       const refresh_token = await jwt.sign(
@@ -59,7 +61,9 @@ router.post("/login", async (req, res, next) => {
           user_id: data[0][0].user_id,
         },
         process.env.jwt_secret,
-        {expiresIn: "14d"}
+        {
+          expiresIn: "14d",
+        }
       );
 
       res.cookie("access_token", access_token, {
@@ -124,7 +128,9 @@ router.get("/refresh", async (req, res) => {
           name: user[0].name,
         },
         process.env.jwt_secret,
-        {expiresIn: "1h"}
+        {
+          expiresIn: "1h",
+        }
       );
 
       res.cookie("access_token", access_token, {
@@ -137,8 +143,56 @@ router.get("/refresh", async (req, res) => {
         refresh_token: refresh_token,
       });
     } catch (e) {
-      res.status(401).send({msg: "retry login"});
+      res.status(401).send({
+        msg: "retry login",
+      });
     }
+  }
+});
+
+router.get("/find_id", async (req, res, next) => {
+  let {name, agency, phone_number, ver_code} = req.query;
+  let result_code = 404;
+  let message = "에러가 발생했습니다.";
+  let [find_id] = "";
+  let user_id, user_name;
+  try {
+    let [val_num] = await pool.execute(
+      `SELECT * FROM sms_validation WHERE phone_number = ? and code = ?`,
+      [phone_number, ver_code]
+    );
+
+    if (val_num[0] == null) {
+      result_code = 400;
+      message = "유효하지 않는 코드입니다.";
+      user_id = "";
+      user_name = "";
+    } else {
+      [find_id] = await pool.execute(
+        `SELECT user_id, name FROM user WHERE name = ? and agency = ? and phone_number = ?`,
+        [name, agency, phone_number]
+      );
+      if (find_id[0] == null) {
+        result_code = 401;
+        message = "존재하지 않는 사용자 정보입니다.";
+        user_id = "";
+        user_name = "";
+      } else {
+        result_code = 200;
+        message = "아이디를 조회했습니다.";
+        user_id = find_id[0].user_id;
+        user_name = find_id[0].name;
+      }
+    }
+    return res.json({
+      code: result_code,
+      message: message,
+      user_id: user_id,
+      name: user_name,
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json(err);
   }
 });
 
