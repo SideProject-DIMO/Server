f1448b9f8a9b71dbbca05bc489be2129df7a010d
plum-king <jeesoo0908@gmail.com>
2024-11-19 02:15:25 +0900

    테이블 생성 함수 변경 (#136)

--- dimo.sql
+++ dimo.sql
@@ -153,7 +153,7 @@ CREATE TABLE `request_character` (
   `title` varchar(50) NOT NULL,
   `character_name` varchar(100) NOT NULL,
   PRIMARY KEY (`request_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- 작성자 신고하기
 CREATE TABLE `report_user` (
@@ -163,7 +163,7 @@ CREATE TABLE `report_user` (
   `report_reason` varchar(100) NOT NULL,
   `review_id` int(11) NOT NULL,
   PRIMARY KEY (`report_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- 리뷰 가리기
 CREATE TABLE `blind_review` (
@@ -173,7 +173,7 @@ CREATE TABLE `blind_review` (
   `user_id` varchar(30) NOT NULL,
   `character_id` int(11) NOT NULL,
   PRIMARY KEY (`blind_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- 최근 검색어 저장
 CREATE TABLE `search_list` (
